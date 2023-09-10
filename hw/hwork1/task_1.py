974d9afdf4f94451cda2239c036e170f04677cb5
sergey <sergey.fomin.90@inbox.ru>
2023-09-10 14:52:26 +0400

    the files for task_1 for HW #1 was completed

--- hw/hwork1/task_1.py
+++ hw/hwork1/task_1.py
@@ -1,3 +1,12 @@
+"""Домашнее задание.
+Задание 1.
+В классе Calculator создайте метод calculateDiscount, который принимает сумму
+покупки и процент скидки и возвращает сумму с учетом скидки.
+Ваша задача - проверить этот метод с использованием библиотеки AssertJ.
+Если метод calculateDiscount
+получает недопустимые аргументы, он должен выбрасывать исключение ArithmeticException.
+Не забудьте написать тесты для проверки этого поведения.
+"""
 class Calculator:
     def calculate_discount(self, total_amount, discount_percent):
         if not isinstance(total_amount, (int, float)) or not \
